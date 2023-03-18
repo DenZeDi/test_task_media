10203065cee52655c2dc91661a7b6e4852680651
DenZeDi <dskostrov@miem.hse.ru>
2023-03-18 14:02:52 +0300

    Almost completed data cleaning and db

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import json
 import pandas as pd
+from sqlalchemy import create_engine
+
+engine = create_engine('sqlite:///df_location.db', echo=False)
 
 with open(r"kfc_locations.json", 'r', encoding='utf8') as read_file:
     data = json.load(read_file)
 
-df_locations = pd.DataFrame(columns=['INDEX', 'CITY', 'ADDRESS', 'COORD', 'STL', 'ETL', 'TF', 'TT', 'F'])
+df_locations = pd.DataFrame(columns=['INDEX', 'CITY', 'ADDRESS', 'LAT_COORD', 'LONG_COORD', 'STL', 'ETL', 'TF', 'TT'])
 
 features = set()
 
@@ -14,12 +17,18 @@ for point in data.get('searchResults'):
         if len(point['storePublic']['contacts']['streetAddress']['ru'].split(', ')) > 1:
             address_list = point['storePublic']['contacts']['streetAddress']['ru'].split(', ')
 
-            index = address_list[0]
-            city = address_list[1]
-            temp_addr = address_list[2:]
-            address = ", ".join(temp_addr)
+            if address_list[0].isdigit():
+                index = address_list[0]
+                city = address_list[1]
+                address = ", ".join(address_list[2:]).replace('\n', '')
+            else:
+                index = None
+                city = address_list[0]
+                address = ", ".join(address_list[1:]).replace('\n', '')
 
         coord = point['storePublic']['contacts']['coordinates']['geometry']['coordinates']
+        lat_coord = coord[0]
+        long_coord = coord[1]
 
         stl = point['storePublic']['openingHours']['regular']['startTimeLocal']
         etl = point['storePublic']['openingHours']['regular']['endTimeLocal']
@@ -36,8 +45,15 @@ for point in data.get('searchResults'):
         for feature in point['storePublic']['features']:
             features.add(feature)
 
-        data_row.extend([index, city, address, coord, stl, etl, tf, tt, f])
+        data_row.extend([index, city, address, lat_coord, long_coord, stl, etl, tf, tt])
 
         df_locations.loc[len(df_locations)] = data_row
 
+df_locations.to_csv('df_location', index=False)
+
+# df_locations.to_sql(name='df_location', con=engine, index=False, if_exists='replace')
+
 print(df_locations)
+
+
+
